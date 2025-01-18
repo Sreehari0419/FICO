fd463da6c6a29dd726b28975c77da0f1915610a3
Haribabu <bigdata.mastery@gmail.com>
2025-01-18 15:57:35 +0530

    matrix func is added

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+  
+def create_matrix(row, col):
+    """This function creates a matrix filled with zeros"""
+    matrix = [[0 for x in range(col)] for y in range(row)]
+    return matrix
