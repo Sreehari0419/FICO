d0cbc512f1a65f6fd428c4bc98d0346ffd1e471a
Haribabu <bigdata.mastery@gmail.com>
2025-01-18 16:18:03 +0530

    Two lines have been deleted

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def factorial(n):
-    """This function returns the factorial of a given integer"""
+    
     print("welcome")
     if n == 0:
         return 1
@@ -7,7 +7,7 @@ def factorial(n):
         return n * factorial(n-1)
   
 def create_matrix(row, col):
-    """This function creates a matrix filled with zeros"""
+ 
     matrix = [[0 for x in range(col)] for y in range(row)]
     return matrix
 
